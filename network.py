f73d681fd5f1495e3dd9c7553de181f490b9e28a
Henry Lee <gbvrcx@gmail.com>
2019-11-11 22:26:58 +0900

    Write basic classes
    Write basic structure of model.

--- network.py
+++ network.py
@@ -1,3 +1,32 @@
 from tensorflow.keras.applications.vgg19 import VGG19
 
 import tensorflow.keras as keras
+import tensorflow.keras.layers as layers
+
+class Encoder(keras.Model):
+  def __init__(self, name='encoder', **kwargs):
+    super(Encoder, self).__init__(name=name, **kwargs)
+
+  def call(self):
+    raise NotImplemented
+
+class Decoder(keras.Model):
+  def __init__(self, name='decoder', **kwargs):
+    super(Decoder, self).__init__(name=name, **kwargs)
+
+  def call(self):
+    raise NotImplemented
+
+class AdaIN(layers.Layer):
+  def __init__(self, name='adain', **kwargs):
+    super(AdaIN, self).__init__(name=name, **kwargs)
+
+  def call(self):
+    raise NotImplemented
+
+class Stylizer(keras.Model):
+  def __init__(self, name='stylizer', **kwargs):
+    super(Stylizer, self).__init__(name=name, **kwargs)
+
+  def call(self):
+    raise NotImplemented
