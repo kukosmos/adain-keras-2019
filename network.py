68eb25005932568656923f49b693673de59fe7d1
Henry Lee <gbvrcx@gmail.com>
2019-11-14 13:09:55 +0900

    Implement encoder
    Implement encoder with pretrained VGG19
    that provided by keras.

--- network.py
+++ network.py
@@ -4,11 +4,16 @@ import tensorflow.keras as keras
 import tensorflow.keras.layers as layers
 
 class Encoder(keras.Model):
-  def __init__(self, name='encoder', **kwargs):
+  def __init__(self, name='encoder', encoder_layers=['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1'], input_shape=(None, None, 3), **kwargs):
+    assert len(encoder_layers) > 0, 'No "encoder_layers" is provided.'
+    
     super(Encoder, self).__init__(name=name, **kwargs)
+    vgg = VGG19(input_tensor=layers.Input(shape=input_shape))
+    output_layers = [vgg.get_layer(layer_name).output for layer_name in encoder_layers]
+    self.encoder = keras.Model(inputs=vgg.input, outputs=output_layers)
 
-  def call(self):
-    raise NotImplemented
+  def call(self, x):
+    return self.encoder(x)
 
 class Decoder(keras.Model):
   def __init__(self, name='decoder', **kwargs):
